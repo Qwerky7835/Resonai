5a81cd9dd142d33ef8c0ec7e93cdda566766a3d4
TC <tianchansa@gmail.com>
2023-09-23 09:17:59 +0200

    Personal Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Daily from './components/daily.js'
+import Personal from './components/personal.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Daily />
+        <Personal />
       </header>
     </div>
   );
